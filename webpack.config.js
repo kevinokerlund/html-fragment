227b14ad6a18ad2ad956ee6b62f82b3f96c66481
Kevin Okerlund <kevinokerlund@users.noreply.github.com>
2018-11-28 12:03:17 -0700

    Switch to yarn, add jest, update to webpack 4, use .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,14 @@
-var webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
+	mode: 'production',
 	entry: './src/html-fragment.js',
 	devtool: 'source-map',
 	output: {
-		path: './lib',
+		path: path.resolve(__dirname, 'lib'),
 		filename: 'html-fragment.min.js',
 		library: 'HtmlFragment',
 		libraryTarget: 'umd',
-		umdNamedDefine: true
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'babel',
-				test: /\.js$/,
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015'],
-					plugins: ['babel-plugin-add-module-exports']
-				}
-			}
-		]
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			},
-			output: {
-				comments: false
-			}
-		})
-	]
+		libraryExport: 'default',
+	}
 };
